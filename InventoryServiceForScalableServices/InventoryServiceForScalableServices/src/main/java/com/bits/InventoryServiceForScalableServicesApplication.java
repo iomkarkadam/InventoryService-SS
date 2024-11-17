0fc55f95228a7f4b84872251bf145e47ff035232
iomkarkadam <omkarsk.0101@gmail.com>
2024-11-17 17:02:29 +0530

    initial commit- added simple springboot app

---
+++ InventoryServiceForScalableServices/InventoryServiceForScalableServices/src/main/java/com/bits/InventoryServiceForScalableServicesApplication.java
@@ -0,0 +1,13 @@
+package com.bits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceForScalableServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceForScalableServicesApplication.class, args);
+	}
+
+}
